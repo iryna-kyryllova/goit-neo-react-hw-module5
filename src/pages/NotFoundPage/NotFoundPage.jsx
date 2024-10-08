38a419cdbcbf1b45ab3991a5c5354cafe7f8177f
Iryna Kyryllova <keira.kirillova@gmail.com>
2024-10-08 17:01:31 +0200

    fix 404 page, add versel settings

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,18 @@
+import { Link } from 'react-router-dom'
+import { MdOutlineKeyboardBackspace } from 'react-icons/md'
 import NotFoundImg from 'img/not_found.svg'
 import styles from './NotFoundPage.module.css'
 
 const NotFoundPage = () => {
   return (
-    <div className={styles.content}>
-      <img src={NotFoundImg} className={styles.img} alt='Page not found' />
+    <div>
+      <Link to='/' className={styles.back}>
+        <MdOutlineKeyboardBackspace />
+        Go Home
+      </Link>
+      <div className={styles.imgHolder}>
+        <img src={NotFoundImg} className={styles.img} alt='Page not found' />
+      </div>
     </div>
   )
 }
